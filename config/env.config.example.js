7095cbbffe9866affafbc3a72bd2be810e5657d1
weiyingHappy <949736522@qq.com>
2019-02-14 14:53:20 +0800

    配置文件

---
+++ config/env.config.example.js
@@ -0,0 +1,10 @@
+const define = {
+  OA_PATH: '',
+  // "OA_CRM_UPLOAD": "http://112.74.177.132:8003/admin/",
+  OA_CLIENT_ID: '',
+  OA_CLIENT_SECRET: '',
+  AUTH_NAME: '',
+  TOKEN_PREFIX: '',
+  UPLOAD_PATH: '',
+};
+export default define;
